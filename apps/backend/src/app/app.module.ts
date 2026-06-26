b7ad91677c4ed6d9c73c4ba8af74c5bcf353ec96
Swedka121 <ivan.hryshchuk2011@gmail.com>
2026-06-27 02:03:37 +0300

    FIA-5 FIA-10 feat: Added some features to nestjs skeleton as config, scalar docs, added docker-build target for nx and provide Dockerbuild file, changed .gitignore to handle .env files in entire project

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from '../config/app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig],
+      envFilePath: ['.env.local', '.env'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,15 +3,41 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.enableCors();
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: 'v1',
+  });
   const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('FIA_Hackhathon')
+    .setDescription('The fia_hackhathon api documentation')
+    .setVersion('v1')
+    .addBearerAuth({ type: 'http' })
+    .setLicense(
+      'MIT',
+      'https://github.com/KryaKryaTeam/fia_hackathon?tab=MIT-1-ov-file',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  app.use(
+    '/reference',
+    apiReference({ theme: 'purple', content: document, hideModels: true }),
+  );
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
