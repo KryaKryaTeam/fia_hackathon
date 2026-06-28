73f91ca0bc8cfff2e6226575376f9005da5fa579
Swedka121 <ivan.hryshchuk2011@gmail.com>
2026-06-29 01:09:40 +0300

    fix: fixed ci errors

--- apps/mlService/mlService/redis_queue.py
+++ apps/mlService/mlService/redis_queue.py
@@ -1,11 +1,15 @@
-import redis
 import json
 import os
+
+import redis
 from dotenv import load_dotenv
 
 load_dotenv()
 
-r = redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), decode_responses=True)
+r = redis.Redis(
+    host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), decode_responses=True
+)
+
 
 def push_event(event: dict):
     r.rpush(os.getenv("QUEUE_NAME"), json.dumps(event))
